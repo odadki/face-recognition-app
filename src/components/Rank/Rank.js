8f58c683c6603bd5a6fdf1bafa835543af0b272f
odadki <93558662+odadki@users.noreply.github.com>
2022-06-07 20:26:00 -0700

    Added initial files

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f3">{"Danielle, Your rank is..."}</div>
+      <div className="f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
